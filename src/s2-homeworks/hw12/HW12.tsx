29e7f08e357e57867ba42b389af61acb32f08992
krylov72 <qobject1337@gmail.com>
2024-09-20 13:02:51 +0500

    add logic to function send

--- src/s2-homeworks/hw12/HW12.tsx
+++ src/s2-homeworks/hw12/HW12.tsx
@@ -1,58 +1,57 @@
-import React, {useEffect} from 'react'
-import s from './HW12.module.css'
-import s2 from '../../s1-main/App.module.css'
-import SuperSelect from '../hw07/common/c5-SuperSelect/SuperSelect'
-import {useDispatch, useSelector} from 'react-redux'
-import {changeThemeId} from './bll/themeReducer'
-import { AppStoreType, useAppSelector } from '../hw10/bll/store'
+import React, { useEffect } from "react";
+import s from "./HW12.module.css";
+import s2 from "../../s1-main/App.module.css";
+import SuperSelect from "../hw07/common/c5-SuperSelect/SuperSelect";
+import { useDispatch, useSelector } from "react-redux";
+import { changeThemeId } from "./bll/themeReducer";
+import { AppStoreType, useAppSelector } from "../hw10/bll/store";
 
 /*
-* 1 - в файле themeReducer.ts написать нужные типы вместо any, дописать редьюсер
-* 2 - получить themeId из редакса
-* 3 - дописать тип и логику функции change
-* 4 - передать пропсы в SuperSelect
-* */
+ * 1 - в файле themeReducer.ts написать нужные типы вместо any, дописать редьюсер
+ * 2 - получить themeId из редакса
+ * 3 - дописать тип и логику функции change
+ * 4 - передать пропсы в SuperSelect
+ * */
 
-
-
-const themes = [
-    {id: 1, value: 'light'},
-    {id: 2, value: 'blue'},
-    {id: 3, value: 'dark'},
-]
+const themes: Array<{ id: number; value: string }> = [
+  { id: 1, value: "Default" },
+  { id: 2, value: "Blue" },
+  { id: 3, value: "Dark" },
+];
 
 const HW12 = () => {
+  const themeId = useSelector<AppStoreType, number>(
+    (state) => state.theme.themeId
+  );
+  const dispatch = useDispatch();
+  // взять ид темы из редакса
 
-    const themeId = useAppSelector(state => state.theme.themeId)
-    const dispatch = useDispatch()
-    // взять ид темы из редакса
+  const change = (id: string) => {
+    // дописать функцию
+    dispatch(changeThemeId(parseInt(id)));
+  };
 
+  useEffect(() => {
+    document.documentElement.dataset.theme = themeId + "";
+  }, [themeId]);
 
-    const change = (id: number) => { // дописать функцию
-        dispatch(changeThemeId(id))
-    }
+  return (
+    <div id={"hw12"}>
+      <div id={"hw12-text"} className={s2.hwTitle}>
+        Homework #12
+      </div>
 
-    useEffect(() => {
-        document.documentElement.dataset.theme = themeId + ''
-    }, [themeId])
+      <div className={s2.hw}>
+        <SuperSelect
+          id={"hw12-select-theme"}
+          className={s.select}
+          value={themeId}
+          onChangeOption={change}
+          options={themes}
+        />
+      </div>
+    </div>
+  );
+};
 
-    return (
-        <div id={'hw12'}>
-            <div id={'hw12-text'} className={s2.hwTitle}>
-                Homework #12
-            </div>
-
-            <div className={s2.hw}>
-                <SuperSelect
-                    id={'hw12-select-theme'}
-                    className={s.select}
-                    value={themeId}
-                    onChangeOption={change}
-                    options={themes}
-                />
-            </div>
-        </div>
-    )
-}
-
-export default HW12
+export default HW12;
